27407e393130e1c8714c773c120fe2862119f953
Andrea Verdirrame <andreaverdirrame11@gmail.com>
2026-02-02 22:45:28 +0100

    #update to register some habits

--- HABITTRACKER/Habit-tracker/Program.cs
+++ HABITTRACKER/Habit-tracker/Program.cs
@@ -25,6 +25,33 @@ namespace Habit_tracker
 
                 tableCmd.ExecuteNonQuery();
                 connection.Close();
+
+                #region To register some habits 
+
+                (string Col1, string Col2)[] arrayDb = new (string, string)[]
+                    { ("DRINKING COFFEE", "NUMBER OF CUPS"),
+                      ("READING BOOKS", "NUMBER OF PAGES"),
+                      ("RUNNING", "KILOMETERS"),
+                      ("MEDITATION", "MINUTES"),
+                      ("WATER INTAKE", "LITERS") };
+
+                connection.Open();
+
+                foreach (var dbArray in arrayDb)
+                {
+                    string query =
+                    $"INSERT INTO Register_Habit (Name_Habit, Unit_Of_Measurement) VALUES(@val1, @val2)";
+
+                    using (var command = new SqliteCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@val1", dbArray.Col1); // Name_Habit
+                        command.Parameters.AddWithValue("@val2", dbArray.Col2); // Unit_Of_Measurement
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+                #endregion
             }
 
             Menu.GetUserInput();
