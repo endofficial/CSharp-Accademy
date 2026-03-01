80227e97bd9fdb48d9e4696798a105babba496f1
Andrea Verdirrame <andreaverdirrame11@gmail.com>
2026-03-01 17:37:28 +0100

    #update

---
+++ IntermediateProject/TCSA.OOP.LibraryManagementSystem/Enums.cs
@@ -0,0 +1,14 @@
+﻿namespace TCSA.OOP.LibraryManagementSystem;
+
+    //Un enum consente di definire un tipo che può accettare solo uno dei valori predefiniti.
+    //Questo limita i possibili valori che una variabile può contenere, riducendo gli errori.
+    //Sono particolarmente adatti per situazioni in cui si hanno solo poche opzioni.
+    internal class Enums
+{
+    internal enum MenuOptions
+    {
+        ViewBooks,
+        AddBook,
+        DeleteBook
+    }
+}
--- IntermediateProject/TCSA.OOP.LibraryManagementSystem/UserInterface.cs
+++ IntermediateProject/TCSA.OOP.LibraryManagementSystem/UserInterface.cs
@@ -62,15 +62,3 @@ internal static class MockDatabase
     };
 }
 
-//Un enum consente di definire un tipo che può accettare solo uno dei valori predefiniti.
-//Questo limita i possibili valori che una variabile può contenere, riducendo gli errori.
-//Sono particolarmente adatti per situazioni in cui si hanno solo poche opzioni.
-internal class Enums
-{
-    internal enum MenuOptions
-    {
-        ViewBooks,
-        AddBook,
-        DeleteBook
-    }
-}
