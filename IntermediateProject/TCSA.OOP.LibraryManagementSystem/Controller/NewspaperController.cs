24dbc7a78b0fd85c16f05331c6b5d592cae43933
Andrea Verdirrame <andreaverdirrame11@gmail.com>
2026-03-01 19:52:43 +0100

    #add new class to newspaper

---
+++ IntermediateProject/TCSA.OOP.LibraryManagementSystem/Controller/NewspaperController.cs
@@ -0,0 +1,20 @@
+﻿using Spectre.Console;
+using TCSA.OOP.LibraryManagementSystem.Controllers;
+using TCSA.OOP.LibraryManagementSystem.Models;
+
+namespace TCSA.OOP.LibraryManagementSystem.Controller;
+
+internal class NewspaperController : IBaseController
+{
+    public void ViewItems()
+    { 
+    }
+
+    public void AddItems()
+    { 
+    }
+
+    public void DeleteItems()
+    { 
+    }
+}
