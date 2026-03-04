44b88702978b11ac9714f9781cfb31afdff22767
Andrea Verdirrame <andreaverdirrame11@gmail.com>
2026-03-04 23:36:51 +0100

    #update

--- CodingTracker/CodingTracker/Controller/InputInsert.cs
+++ CodingTracker/CodingTracker/Controller/InputInsert.cs
@@ -24,15 +24,16 @@ internal class InputInsert
     public static bool GetTimeSessionInput()
     {
         string durationInput = AnsiConsole.Prompt(
-            new TextPrompt<string>("[grey bold]Please insert the duration (Format: [green]hh:mm[/]) or type [yellow]0[/] to return to main menu.")
+            new TextPrompt<string>("[bold]Please insert the duration (Format: [green]hh:mm[/]) or type [yellow]0[/] to return to main menu.[/]")
             .Validate(input =>
             {
                 if (input == "0") return ValidationResult.Success();
 
-                bool isValid = TimeSpan.TryParseExact(input, @"h/:mm", CultureInfo.InvariantCulture, out var time);
+                string[] format = { @"h\:mm", @"hh\:mm" };
+                bool isValid = TimeSpan.TryParseExact(input, format, CultureInfo.InvariantCulture, out var time);
 
                 // Check if the time is valid and within the range of 0 to 24 hours
-                if (!isValid) return ValidationResult.Error("[red]Duration invalid! Use the time format '[orange]hh:mm[/]'[/]");
+                if (!isValid) return ValidationResult.Error("[red]Duration invalid! Use the time format '[blue]hh:mm[/]'[/]");
 
                 if (time.Ticks < 0) return ValidationResult.Error("[red]Negative time not allowed.[/]");
 
