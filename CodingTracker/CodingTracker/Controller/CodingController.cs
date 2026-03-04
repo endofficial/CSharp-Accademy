eb7005457ee71f8d17033360c96511519590db11
Andrea Verdirrame <andreaverdirrame11@gmail.com>
2026-03-04 23:05:32 +0100

    #add a new method to date register

---
+++ CodingTracker/CodingTracker/Controller/CodingController.cs
@@ -0,0 +1,21 @@
+﻿using Spectre.Console;
+using CodingTracker;
+
+namespace CodingTracker.Controller;
+
+internal class CodingController
+{
+    public static bool RegisterSession()
+    {
+        Clear();
+        
+        bool dateSession = InputInsert.GetDateSessionInput();
+        if (dateSession == false) return false;
+
+        bool durationSession = InputInsert.GetTimeSessionInput();
+        if (durationSession == false) return false;
+
+        return true;
+    }
+}
+
---
+++ CodingTracker/CodingTracker/Controller/InputInsert.cs
@@ -0,0 +1,29 @@
+﻿using Spectre.Console;
+using System.Globalization;
+
+namespace CodingTracker.Controller;
+
+internal class InputInsert
+{
+    public static bool GetDateSessionInput()
+    {
+        AnsiConsole.MarkupLine("Register a new session.");
+        var date = AnsiConsole.Ask<string>("Please enter date (yyyy-MM-dd). You type [red]0[/] to return to main menu.");
+
+        if (date == "0") return false;
+
+        while (!DateTime.TryParseExact(date, "yyyy-MM-dd", new CultureInfo("en-EN"), DateTimeStyles.None, out _))
+        {
+            AnsiConsole.MarkupLine("[red]Invalid date format.[/]");
+            date = AnsiConsole.Ask<string>("Please enter date (yyyy-MM-dd). You type [red]0[/] to return to main menu.");
+            if (date == "0") return false;
+        }
+        return true;
+    }
+
+    public static bool GetTimeSessionInput()
+    {
+        return true;
+    }
+}
+
